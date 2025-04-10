3c8f1a0a28ee70ba31cdabaed00366ca3cdc459e
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2025-04-10 22:42:56 +0000

    v2...

--- static/js/main.js
+++
@@ -1,6 +0,0 @@
-// Main JavaScript file
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("Elevation World website loaded");
-
-  // You can add more interactive functionality here as needed
-});
